8ba0e106ec73e1e3d71192f16b1acb5ae48be0ab
alejik88 <demchukoleg88@gmail.com>
2025-04-15 17:34:57 +0200

    Add posts api call

---
+++ server/api/posts.get.ts
@@ -0,0 +1,16 @@
+import postsData from '~/data/social_posts_large.json'
+import type { Post } from '~/types/posts'
+
+// This is a mock API endpoint that simulates fetching posts data from a server. 
+const getPosts = (): Promise<Post[]> => {
+  return new Promise((resolve => {
+    setTimeout(() => {
+      resolve(postsData as Post[])
+    }, 500)
+  }))
+}
+
+export default defineEventHandler(async () => {
+  const postsData = await getPosts()
+  return postsData
+})
