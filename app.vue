a2f15088e5ee4934f36dbd67311a15b7285afd3b
alejik88 <demchukoleg88@gmail.com>
2025-04-15 15:31:38 +0200

    Add prettier

--- app.vue
+++ app.vue
@@ -1,10 +1,3 @@
-<script lang="ts" setup>
-const sum = (a: number, b: number) => a + b;
-const result = sum(1, 2)
-const str = "123"
-const str2 = 'qwe'
-console.log(result, str, str2); // 3
-</script>
 <template>
   <div>
     <NuxtRouteAnnouncer />
