8d35281b340c00ad8c3797cf3449ee4335faf16e
alejik88 <demchukoleg88@gmail.com>
2025-04-15 20:16:25 +0200

    Add navigation to post page

---
+++ pages/[id].vue
@@ -0,0 +1,3 @@
+<template>
+  <div>I am post page</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,16 @@
 import { PostCard } from '#components'
 
 const { posts } = usePostsStore()
+const navigateToPostPage = (postId: number) => {
+  navigateTo(`/${postId}`)
+}
 </script>
 
 <template>
   <section>
     <div class="flex justify-center items-start flex-wrap">
       <div v-for="post in posts" :key="post.id" class="w-full sm:w-1/2 lg:w-1/3 p-4">
-        <PostCard :post="post" />
+        <PostCard :post="post" @:click="navigateToPostPage(post.id)" />
       </div>
     </div>
   </section>
