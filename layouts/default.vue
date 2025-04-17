9f4792e0e55a18abccde98ba9a0a3bdc13702858
alejik88 <demchukoleg88@gmail.com>
2025-04-17 12:03:24 +0200

    Add spinner preloader

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,9 @@ const postsStore = usePostsStore()
     <AppHeader />
     <main class="my-8 flex justify-center">
       <div class="container">
-        <div v-if="postsStore.loading" class="text-center">Loading</div>
+        <div v-if="postsStore.loading" class="text-center">
+          <img src="/images/spinner.svg" alt="Loading..." class="w-32 h-32 mx-auto mt-12" />
+        </div>
         <slot v-else />
       </div>
     </main>
