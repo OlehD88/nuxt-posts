049069113d275c810c99d4976bcfacf9f6eead81
alejik88 <demchukoleg88@gmail.com>
2025-04-17 12:12:19 +0200

    Add error handler placeholder

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,25 @@
 <script setup lang="ts">
 import AppHeader from '@/components/AppHeader.vue'
 const postsStore = usePostsStore()
+// This is a placeholder for the error handler
+// In a real application, you might want to log the error to an external service
+const errorHandler = (error: unknown) => {
+  console.error('An error occurred:', error)
+}
 </script>
 
 <template>
-  <div>
-    <AppHeader />
-    <main class="my-8 flex justify-center">
-      <div class="container">
-        <div v-if="postsStore.loading" class="text-center">
-          <img src="/images/spinner.svg" alt="Loading..." class="w-32 h-32 mx-auto mt-12" />
+  <NuxtErrorBoundary @error="errorHandler">
+    <div>
+      <AppHeader />
+      <main class="my-8 flex justify-center">
+        <div class="container">
+          <div v-if="postsStore.loading" class="text-center">
+            <img src="/images/spinner.svg" alt="Loading..." class="w-32 h-32 mx-auto mt-12" />
+          </div>
+          <slot v-else />
         </div>
-        <slot v-else />
-      </div>
-    </main>
-  </div>
+      </main>
+    </div>
+  </NuxtErrorBoundary>
 </template>
